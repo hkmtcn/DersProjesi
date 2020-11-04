d3a9f6a9ccba4033bf3590c514c007c02f2ace61
Hikmetcan Özcan <hikmetcan.ozcan@kocaeli.edu.tr>
2020-11-04 12:31:22 +0300

    for döngüsü eklendiProje dosyası ekle.

---
+++ DersProjesi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DersProjesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
